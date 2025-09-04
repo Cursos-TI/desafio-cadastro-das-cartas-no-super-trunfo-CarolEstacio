22574157481cf870726754e6e378dc92ea2d8d22
CarolEstacio <202507365606@alunos.estacio.br>
2025-09-04 01:06:27 +0000

    Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+  #include <stdio.h>
 
  int main(){
 
@@ -7,19 +7,19 @@
  //variáveis e tipos de dados da carta n1:
 
  char Estado1 [20], Cidade1 [20], Codigo1 [20]; // Declara três arrays, cada um de 20 caracteres. 
- int Populacao1, Turistico1;    // declara tipos de dados inteiros. 
- float KM1, Pib1;       // declara tipos de dados flutuantes. 
+ int Turistico1, Populacao1;    // declara tipos de dados inteiros. 
+ float KM1, Pib1, Densidade1, PibPerc1;   // declara tipos de dados flutuantes. 
 
  // variáveis e tipos de dados da carta n2:
 
  char Estado2 [30], Cidade2 [30], Codigo2 [30]; // Declara três arrays, cada um de 30 caracteres. 
- int Populacao2, Turistico2;    // declara tipos de dados inteiros. 
- float KM2, Pib2;       // declara tipos de dados flutuantes. 
+ int Turistico2, Populacao2;    // declara tipos de dados inteiros. 
+ float KM2, Pib2, Densidade2, PibPerc2;    // declara tipos de dados flutuantes. 
 
  //Sintaxe scanf
  //scanf("formato1""formato2", &variavel1, &variavel2,...);
 
- printf("CARTA 1\n"); // Preencha os dados da Carta n1 conforme as orientações:
+ printf("=======CARTA 1==========\n"); // Preencha os dados da Carta n1 conforme as orientações:
 
  printf("Digite o Estado: "); // Insira uma letra de 'A' a 'H' (representando um dos 8 estados).
  scanf("%s", Estado1);
@@ -42,18 +42,28 @@
  printf("Digite o número de pontos turisticos: "); // Insira o número de pontos túristicos dessa cidade.
  scanf("%d", &Turistico1);
 
- //Sintaxe printf
+ //calculo de densidade populacional da carta número 1:
+
+ Densidade1 = (float) (Populacao1 / KM1);
+
+ //calculo do Pib Per capita da carta número1:
+ PibPerc1 =  (float) (Pib1 / Populacao1);
+
+ //Sintaxe print
  //printf ("formato 1", variavel1, variavel1...);
 
+ printf ("==========RESULTADO CARTA N1=====\n");
  printf("O código da carta é:%s\n", Codigo1);
  printf("O Estado é: %s\n", Estado1);
  printf("O nome da cidade é: %s\n", Cidade1);
  printf("A população é de:%d\n", Populacao1);
- printf("A área em km² é:%.6f\n", KM1);
+ printf("A área em km² é:%.3f\n", KM1);
  printf("O Pib é de:%.6f\n", Pib1);
  printf("Os números de pontos túristicos são:%d\n", Turistico1);
+ printf("A densidade Populacional é de:%.6f\n", Densidade1);
+ printf(" O Pib Per Capta é de:%.6f\n", PibPerc1);
 
-  printf("CARTA 2\n"); //Preencha os dados da Carta N2, conforme preenchido a carta N1.
+  printf("========CARTA 2=======\n"); //Preencha os dados da Carta N2, conforme preenchido a carta N1.
 
  printf("Digite o Estado: ");
  scanf("%s", Estado2);
@@ -76,13 +86,24 @@
  printf("Digite o número de pontos turisticos: "); // Insira o número de pontos túristicos dessa cidade.
  scanf("%d", &Turistico2);
 
+ //calculo de densidade populacional da carta número 2:
+
+ Densidade2 = (float) (Populacao2 / KM2);
+
+ //calculo do Pib Per capita da carta número2:
+ PibPerc2 = (float) (Pib2 / Populacao2);
+
+ printf(" ======RESULTADOS DA CARTA N2======\n");
  printf("O código da carta é:%s\n", Codigo2);
  printf("O Estado é: %s\n", Estado2);
  printf("O nome da cidade é: %s\n", Cidade2);
  printf("A população é de:%d\n", Populacao2);
- printf("A área em km² é:%.6f\n", KM2);
+ printf("A área em km² é:%.3f hab/km \n", KM2);
  printf("O Pib é de:%.6f\n", Pib2);
  printf("Os números de pontos túristicos são:%d\n", Turistico2);
+ printf("A densidade Populacional é de: %.6f\n", Densidade2);
+ printf(" O Pib Per Capta é de:%.6f\n", PibPerc2);
+
 
 
  /*  ESPECIFICADORES DE FORMATO E SEUS MODIFICADORES:
@@ -94,7 +115,6 @@
  %s: Imprime uma cadeia (string) de caracteres. 
  */
 
- 
-  return 0;
+ return 0;
 
  }
